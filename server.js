1131da06f225549a71f266c4c529888148084fa4
Abdul Muksith Rizvi <57699415+abdulmuksith3@users.noreply.github.com>
2022-01-02 22:20:01 +0300

    update

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-// app.use(cors());
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+app.use(cors());
+// app.use(function (req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 const buyerRoute = require('./routes/buyer')
 app.use('/buyer', buyerRoute);
